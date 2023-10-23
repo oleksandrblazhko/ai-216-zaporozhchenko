f8e8058c6ad0700b459b7fd89ec98c6d59553c81
mudack <102027756+mudack@users.noreply.github.com>
2023-10-23 15:25:48 +0300

    Update RelationDB_DetailedDataSchema.sql

--- 2-SoftwareDesign/2.4-DetailedDataSchema/RelationDB_DetailedDataSchema.sql
+++ 2-SoftwareDesign/2.4-DetailedDataSchema/RelationDB_DetailedDataSchema.sql
@@ -6,16 +6,19 @@ CREATE TABLE operator
 CREATE TABLE customer
 (
     customer_id INT PRIMARY KEY,
-    telephone INT,
+    telephone INT UNSIGNED NOT NULL,
     CONSTRAINT chk_telephone_length CHECK (LENGTH(telephone::TEXT) = 12),
+    email VARCHAR(1000) CHECK (email ~* '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}$'),
     operator_id INT REFERENCES operator(operator_id)
 );
 
-CREATE TABLE purifier 
+CREATE TABLE food 
 (
-    purifier_id INT PRIMARY KEY,
+    food_id INT PRIMARY KEY,
     name VARCHAR(100),
-    cost INT UNSIGNED,
+    quantity INT UNSIGNED NOT NULL,
+    weight INT UNSIGNED NOT NULL,
+    cost INT UNSIGNED NOT NULL,
     customer_id INT REFERENCES customer(customer_id)
 );
 
@@ -24,10 +27,12 @@ CREATE TABLE user
     user_id INT PRIMARY KEY,
     num_card INT,
     CONSTRAINT chk_num_card_length CHECK (LENGTH(num_card::TEXT) = 16),
-    valid_per TEXT,
-    CONSTRAINT chk_valid_per_format CHECK (valid_per ~ '^(0[1-9]|1[0-2])/[0-9][0-9]$' AND RIGHT(valid_per, 2)::INT + 2000 >= 2023),
+    valid_per TEXT, --тип даних саме TEXT, тому що формат "ММ/РР" не є стандартним форматом для типу даних DATE в SQL.  Тип даних DATE відображає дату в стандартному форматі "РРРР-ММ-ДД" (рік-місяць-день).
+    CONSTRAINT chk_valid_per_format CHECK (valid_per ~ '^(0[1-9]|1[0-2])/[0-9][0-9]$' AND RIGHT(valid_per, 2)::INT + 2000 >= 2023), 
     cvv2 INT,
     CONSTRAINT chk_cvv2_length CHECK (LENGTH(cvv2::TEXT) = 3),
+    pin_code INT,
+    CONSTRAINT chk_pin_code_length CHECK (LENGTH(cvv2::TEXT) = 4),
     customer_id INT UNIQUE,
     FOREIGN KEY (customer_id) REFERENCES customer(customer_id)
 );
