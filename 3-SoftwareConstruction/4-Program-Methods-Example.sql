7f0359efef3819677597663a28730d1553fb87e8
mudack <102027756+mudack@users.noreply.github.com>
2023-11-23 21:44:44 +0200

    Update 4-Program-Methods-Example.sql

--- 3-SoftwareConstruction/4-Program-Methods-Example.sql
+++ 3-SoftwareConstruction/4-Program-Methods-Example.sql
@@ -1,10 +1,10 @@
-CREATE OR REPLACE FUNCTION order_food ( --функція не називається "order", тому що це слово є ключовим для oracle
+CREATE OR REPLACE FUNCTION order_ ( 
     f_name IN VARCHAR2,
     f_quantity IN INT,
     f_weight IN INT,
     f_cost IN INT
 )
-RETURN BOOLEAN IS
+RETURN INT IS
     v_name food.name%Type := upper(f_name);
     v_quantity food.quantity%Type := f_quantity;
     v_weight food.weight%Type := f_weight;
@@ -12,14 +12,23 @@ RETURN BOOLEAN IS
     v_food_id food.food_id%Type;
 BEGIN
     IF LENGTH(v_name) > 100 THEN
-        RETURN FALSE;
-
+        RETURN -1;
     END IF;
+    IF v_quantity < 0 OR v_quantity = 0 THEN
+        RETURN -2;
+    END IF;
+    IF v_weight < 0 OR v_weight = 0 THEN
+        RETURN -3;
+    END IF;
+    IF v_cost < 0 OR v_cost = 0 THEN
+        RETURN -4;
+    END IF;
+
     BEGIN
         SELECT food_id INTO v_food_id FROM food WHERE TRIM (upper(name)) = v_name AND quantity = v_quantity AND weight = v_weight AND cost = v_cost;
-        RETURN TRUE;
+        RETURN 1;
     EXCEPTION WHEN NO_DATA_FOUND THEN
-        RETURN FALSE;
+        RETURN 0;
     END;
 END;
 /
