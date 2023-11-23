3ec0d2e4fabe399430e1b03f260e858ba0db73a7
mudack <102027756+mudack@users.noreply.github.com>
2023-11-23 21:45:52 +0200

    Update 5-Program-Methods-Using-Example.sql

--- 3-SoftwareConstruction/5-Program-Methods-Using-Example.sql
+++ 3-SoftwareConstruction/5-Program-Methods-Using-Example.sql
@@ -1,13 +1,15 @@
 DECLARE
-    v_result BOOLEAN;
+    v_result INT;
 BEGIN
-    v_result := order_food('Борщ', 1, 250, 500);
+    v_result := order_('Борщ', 1, 250, 500);
 
-    IF v_result THEN
+    IF v_result = 1 THEN
         DBMS_OUTPUT.PUT_LINE(':)');
         DBMS_OUTPUT.PUT_LINE('Оплата успішно проведена!');
+        DBMS_OUTPUT.PUT_LINE(v_result);
     ELSE
         DBMS_OUTPUT.PUT_LINE('Страву не знайдено!');
+        DBMS_OUTPUT.PUT_LINE(v_result);
     END IF;
 END;
 /
